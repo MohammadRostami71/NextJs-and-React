0a668cfb24140721b05eb5c9e98690051a259efc
mrostami0 <mohammad.rostami13@gmail.com>
2022-03-01 01:57:33 +0330

    01-Getting Started

---
+++ 01-getting-started/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <h1>hello nextJs</h1>
+            <h2>enjoy develop</h2>
+        </div>
+    )
+}
